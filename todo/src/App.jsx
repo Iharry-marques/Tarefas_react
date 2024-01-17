8daf0269814ec0814f02a7fba2217832a243068d
Your Name <fabio.marques.melo@gmail.com>
2024-01-16 21:22:34 -0300

    quasela

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-
-import Todo from './components/Todo'
-
-import './App.css';
+import Filter from "./components/Filter";
+import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
+
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,32 +28,70 @@ function App() {
     },
   ]);
 
-  const addTodo = (text, category) => {
+  const [search, setSearch] = useState("");
 
-    const newTodos = [...todos, {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
-    },
-  ];
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
 
     setTodos(newTodos);
   };
 
   const removeTodo = (id) => {
-    
-  }
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
 
-  return <div className="app">
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo}/>
-      ))}
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} />
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
     </div>
-    <TodoForm addTodo={addTodo} />
-  </div>
+  );
 }
 
 export default App;
